8f6df46712e8e0d34740da1aaeeaf0712bb34a35
nil & Bill Brinkert <pair+nilthacker.silverfox70@devbootcamp.com>
2016-01-19 15:38:54 -0800

    Implement item editing

--- shoppe/app/controllers/items_controller.rb
+++ shoppe/app/controllers/items_controller.rb
@@ -8,14 +8,31 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
   end
 
   def create
     @item = Item.new(item_params)
-    @item.price = @item.price * 100
-    p @item.price
-    @item.save
-    redirect_to @item
+    p @item
+    if @item.save
+      redirect_to @item
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render 'edit'
+    end
   end
 
   private
