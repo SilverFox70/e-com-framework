70c05253248f480582663f9af755796db50fef61
nil & Bill Brinkert <pair+nilthacker.silverfox70@devbootcamp.com>
2016-01-19 19:41:50 -0800

    Remove welcome controller and views

--- shoppe/config/routes.rb
+++ shoppe/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
   resources :items
   resources :users do
@@ -11,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
