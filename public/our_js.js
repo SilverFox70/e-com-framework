6cf9c7b85711780939dd0be1e67a072a9c1732c9
SilverFox70 <wfbrinkert@gmail.com>
2016-01-23 13:25:56 -0800

    Add delete button to show partial of cart_item view; Add listener for button in our_js.js file; Stub out method for ajax call in our_js; Add destroy method in cart_items controller;TO DO: add jQuery to dynamically remove item from cart view partial

--- public/our_js.js
+++ public/our_js.js
@@ -1,12 +1,13 @@
 
 $(document).ready(function(){
-  console.log("HEY YO")
+  console.log("JS is active...")
   bindListeners();
 });
 
 var bindListeners = function(){
   $("form[action='/cart_items']").on("submit", buyNow);
-  $(".dropdown").on('click', 'a.qty', updateOrderQty)
+  $(".dropdown").on('click', 'a.qty', updateOrderQty);
+  $("#shopping-cart").on('click', 'a.remove-button', deleteItemLine);
 };
 
 var buyNow = function(event){
@@ -15,7 +16,7 @@ var buyNow = function(event){
   console.log("this: " + this);
   console.log(formData)
   var ajaxCall = $.ajax({
-    method: 'post',
+    method: 'POST',
     url: '/cart_items',
     data: formData,
     success: function(response){
@@ -30,10 +31,22 @@ var updateOrderQty = function(event){
   event.preventDefault();
   var this_path = $(this).attr('href')
   $.ajax({
-    method: 'put',
+    method: 'PUT',
     url: "/cart_items" + this_path
   }).done(function(response){
     $('#dropdownMenu1').html(response)
+  });
+};
+
+var deleteItemLine = function(event){
+  event.preventDefault();
+  var this_path = $(this).attr('href');
+  console.log("Got delete line item request: " + this_path);
+  $.ajax({
+    method: 'DELETE',
+    url: this_path,
+  }).done(function(response){
+    console.log("returned from cart_items controller: " + response);
   });
 };
 
