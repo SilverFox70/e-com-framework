b55a684dfd4bfa5b3dead9f78a5f8b64197b3f13
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-01-24 21:30:48 -0800

    Revert "Revert "Add total tracking to cart""

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,8 @@ class CartItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :item
   has_one :user, through: :cart
+
+  def total_price
+    return self.item.price * self.quantity
+  end
 end
